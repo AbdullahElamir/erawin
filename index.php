2a4e552c980f52d08d2304b8990746acea941479
abdullah <abdullah.el_ameer@yahoo.com>
2017-05-23 22:24:32 +0200

    all

--- index.php
+++ index.php
@@ -1219,6 +1219,7 @@ SANITIZING THE ENVIRONMENT CANNOT BE LIMITED ONLY TO THE OPERATING ROOM. EVERY A
       </div>
       </div>
       <div class="col-md-4">
+
       <!-- <h4 class="classic-title"><span>Why Choose Us (suggestion by Osama)</span></h4>
       <div class="panel-group" id="accordion">
       <div class="panel panel-default"> 
@@ -1273,7 +1274,12 @@ SANITIZING THE ENVIRONMENT CANNOT BE LIMITED ONLY TO THE OPERATING ROOM. EVERY A
  
 <h4 class="classic-title"><span>Cleaning and Sanitizing</span></h4>
  
-<div class="fluid-width-video-wrapper" style="padding-top: 56.25%;"><iframe width="560" height="315" src="https://www.youtube.com/embed/5i0hBG8UGA0" frameborder="0" allowfullscreen></iframe></div>
+<div class="fluid-width-video-wrapper" style="padding-top: 56.25%;"><iframe width="560" height="315" src="https://www.youtube.com/embed/5i0hBG8UGA0?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div>
+
+<!-- <object width="425" height="350">
+<param name="movie" value="http://www.youtube.com/v/5i0hBG8UGA0?autoplay=0"></param>
+<embed src="http://www.youtube.com/v/5i0hBG8UGA0?autoplay=0" type="application/x-shockwave-flash" width="425" height="350"></embed>
+</object> -->
 </div>
 </div>
     </div>
